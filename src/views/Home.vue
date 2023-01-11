39d14efea485389f964c8439064305fd1085a941
JinxZhang <737461472@qq.com>
2023-01-11 22:58:29 +0800

    compatible chrome css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,133 +1,112 @@
 <template>
-    <div class="common-layout">
-      <el-divider  style="box-shadow: 1px black solid;" content-position="left">
-      蜀ICP备2022003140号
-      </el-divider>
-      <el-container>
-       <el-aside>
-  
-        <Nav></Nav>
-  
-  
-        </el-aside>
-        
-       
-          <el-main>   <router-view />   </el-main>
+  <div class="common-layout">
+    <el-divider  content-position="left">
+    蜀ICP备2022003140号
+    </el-divider>
+    <el-container>
+     <el-aside>
+
+      <Nav></Nav>
+
+
+      </el-aside>
       
-      </el-container>
-    </div>
-  
-  
-  </template>
-  
-  <script>
+     
+        <el-main>   <router-view />   </el-main>
+    
+    </el-container>
+  </div>
+
+
+</template>
+
+<script>
 
 import Nav from "../components/Nav.vue";
 import useStore from '../store'
-  
-  export default {
 
-      name: "App",
-      components: { Nav },
-      data(){
-        return{
-          isLogin:false,
-          keepLogin:false,
-        }
-      },
-      mounted(){
-        // this.isLogin = useStore().user.getLoginStatus
-        // this.keepLogin = useStore().user.getKeepLoginStatus
-        //   if(this.keepLogin)
-        //     { this.$router.push('/todo')
-        //   }else{
-        //     this.$message.error('请登陆')
-        //      this.$router.push('/login')
-        //   }
-      },
-      beforeRouteEnter(to,from,next){
-        
-          if (useStore().user.getLoginStatus) {
-              next()
-          }else{
-            
-            // this.$message.error('请登陆')
+export default {
+
+    name: "App",
+    components: { Nav },
+    data(){
+      return{
+        isLogin:false,
+        keepLogin:false,
+      }
+    },
+    mounted(){
+      // this.isLogin = useStore().user.getLoginStatus
+      // this.keepLogin = useStore().user.getKeepLoginStatus
+      //   if(this.keepLogin)
+      //     { this.$router.push('/todo')
+      //   }else{
+      //     this.$message.error('请登陆')
+      //      this.$router.push('/login')
+      //   }
+    },
+    beforeRouteEnter(to,from,next){
+      
+        if (useStore().user.getLoginStatus) {
+            next()
+        }else{
           
-            next('/login')
-            
-          }
-      }
-  }
-  </script>
-  
-  <style lang="less" scoped>
-  @import "../assets/fonts/index.css";
-  
-   .el-divider :v-deep{
-        background-color: #b6d7fb;
-        height: 2px;
-  
-   
+          // this.$message.error('请登陆')
         
+          next('/login')
+          
+        }
     }
+}
+</script>
+
+<style lang="less" scoped>
+@import "../assets/fonts/index.css";
+
+html,
+body {
+  font-family: pingfang;
+  height: 100%;
+}
+
+body {
+  overflow-y: hidden;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #F5F7FA;
+
+}
+.common-layout {
+  .el-divider::v-deep {
+    margin: 0;
+    padding: 20px;
+    background-color:rgb(68, 111, 179);
     .el-divider__text.is-left{
-        background-color:#79BBFF;
-  
-        border-radius: 10px;
-        padding:2px 20px;
-        color: #fff;
-        font-weight: lighter;
-        font-size: 14px;
-      }
-  
-  
-    html,body{
-      font-family: pingfang;
-      height: 100%;
+      color: #fff;
+      background-color: rgb(68, 111, 179);
     }
-    body{
-      overflow-y: hidden;
-    }
-  #app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    background-color: #F5F7FA;
-  
   }
-  .el-aside{
-    position: fixed;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 20rem;
-    height: 100%;
-    margin-top: 140px;
-  
-  }
-  
-  .el-main{
-      margin-left: 20rem;
-      width: 100%;
+}
+.el-aside {
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20rem;
+  height: 100%;
+  padding-top: 140px;
+  background-color: #F5F7FA;
+}
 
-      padding: 17px;
-   
-  }
-  
-  
-  /*设置宽度,轨道颜色*/
-  ::-webkit-scrollbar {
-      width: 10px;
+.el-main {
+  margin-left: 20rem;
+  width: 100%;
+  background-color: #F5F7FA;
+  padding: 17px;
 
-      background: #F5F7FA;   /*可以设置轨道颜色*/
-      padding-right: 2px;
-  }
- 
-  /*滚动条*/
-  ::-webkit-scrollbar-thumb {
-      background: #ccc;
-      border-radius: 10px;
-  }
-  
-  </style>
-  
+}
+</style>
